306f93a5b7ab98643a620f746c7bd6f71e550957
banelexhamlashe345 <banelebanele938@gmail.com>
2025-03-19 13:57:00 +0200

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,14 @@
+import { storage } from "./firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+// Upload image to Firebase Storage
+export const uploadImage = async (file) => {
+  try {
+    const storageRef = ref(storage, `images/${file.name}`);
+    await uploadBytes(storageRef, file);
+    return await getDownloadURL(storageRef);
+  } catch (error) {
+    console.error("Error uploading image:", error);
+  }
+};
 
