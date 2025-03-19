8ecc5e21710ef43453e235ee5e3a8c01b24896be
banelexhamlashe345 <banelebanele938@gmail.com>
2025-03-19 13:41:03 +0200

    Update errorHandler.js

--- errorHandler.js
+++ errorHandler.js
@@ -1,1 +1,12 @@
-
+export const handleAuthError = (error) => {
+  switch (error.code) {
+    case "auth/email-already-in-use":
+      return "This email is already in use.";
+    case "auth/wrong-password":
+      return "Incorrect password.";
+    case "auth/user-not-found":
+      return "User not found.";
+    default:
+      return "An error occurred. Try again.";
+  }
+};
