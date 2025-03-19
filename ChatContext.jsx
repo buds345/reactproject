43dc4e50596e796284d01bebaf9033e346d611ef
banelexhamlashe345 <banelebanele938@gmail.com>
2025-03-19 13:38:49 +0200

    Update ChatContext.jsx

--- ChatContext.jsx
+++ ChatContext.jsx
@@ -1,1 +1,15 @@
+import { createContext, useContext, useState } from "react";
 
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [messages, setMessages] = useState([]);
+
+  return (
+    <ChatContext.Provider value={{ messages, setMessages }}>
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChat = () => useContext(ChatContext);
